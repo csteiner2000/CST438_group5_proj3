cbd14af8eb367557585742d29fa084c4e4d76397
Cooper Steiner <csteiner@csumb.edu>
2022-04-20 21:07:16 -0700

    New version

--- index.js
+++ index.js
@@ -1,52 +1,23 @@
-const express = require("express");
-const mysql = require('mysql');
+const express = require('express');
 const app = express();
-const pool = dbConnection();
+const fetch = require('node-fetch');
+
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-
-
-
-app.get('/',  (req, res) => {
- res.render('home')
+app.get('/', (req, res) => {
+  res.render("home");
 });
 
-app.get("/dbTest", async function(req, res){
-let sql = "SELECT CURDATE()";
-let rows = await executeSQL(sql);
-res.send(rows);
-});//dbTest
-
-
-//functions
-async function executeSQL(sql, params){
-return new Promise (function (resolve, reject) {
-pool.query(sql, params, function (err, rows, fields) {
-if (err) throw err; 
-   resolve(rows);
+app.get('/login', (req, res) => { 
+   res.render('login');
 });
+
+app.get('/signup', (req, res) => { 
+   res.render('signup');
 });
-}//executeSQL
-//values in red must be updated
-function dbConnection(){
 
-   const pool  = mysql.createPool({
-
-      connectionLimit: 10,
-      host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-      user: "rromzjicr1yn7h2v",
-      password: "q66nhf8a4xwky6a7",
-      database: "hzu7gr2xk6zbtl8v"
-
-   }); 
-
-   return pool;
-
-} //dbConnection
-
-//start server
 app.listen(3000, () => {
-console.log("Expresss server running...")
-} )
+   console.log('server started');
+});
